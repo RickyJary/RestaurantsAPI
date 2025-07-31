3e03279e2d00ce7f971aca8cc82087cd100666c3
RickJary <ricardogarciajarillo@gmail.com>
2025-07-31 21:45:35 +0200

    fixed: Home

--- controllers/misc.controllers.js
+++ controllers/misc.controllers.js
@@ -1,3 +1,13 @@
 module.exports.getHome = (req, res) => {
-  res.send("API is working");
+  res.json({
+    message: "API is working",
+    endpoints: {
+      login: "POST /login",
+      getAllRestaurants: "GET /restaurants",
+      getRestaurantById: "GET /restaurants/:id",
+      createRestaurant: "POST /restaurants (auth)",
+      updateRestaurant: "PATCH /restaurants/:id (auth)",
+      deleteRestaurant: "DELETE /restaurants/:id (auth)"
+    }
+  });
 };
